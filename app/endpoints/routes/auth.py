c984372de1d5a067b6731351dadd16cc9c5f2afd
Andrey Demianov <gappointgap@gmail.com>
2024-04-20 18:43:00 +0200

    auth models end

--- app/endpoints/routes/auth.py
+++ app/endpoints/routes/auth.py
@@ -3,71 +3,15 @@ from pydantic import BaseModel
 from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from app.core.source import get_user
-from app.src.utils import hash_password, verify_password
+from app.src.utils import authenticate_user, create_access_token, get_current_active_user
 from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from app.endpoints.schema import User, Token, TokenData, UserInDB
-from app.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
-
-
+from app.config import ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    user_dict = get_user_dict(user,username )
-    
-    if not user:
-        return False
-    if not verify_password(password, user[username]["hashed_password"]):
-        return False
-    return user_dict
-
-def get_user_dict(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(login=token_data.username)
-    user_dict = get_user_dict(user,token_data.username )
-    if user is None:
-        raise credentials_exception
-    return user_dict
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 @router.post("/token")
 async def login_for_access_token(
@@ -80,7 +24,7 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
@@ -97,5 +41,5 @@ async def read_users_me(
 async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)],
 ):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+    return [{"owner": current_user.username}]
 
--- app/src/utils.py
+++ app/src/utils.py
@@ -1,6 +1,18 @@
 from passlib.context import CryptContext
+from app.core.source import get_user
+from app.endpoints.schema import User, Token, TokenData, UserInDB
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from typing import Annotated
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, status
+
+from datetime import datetime, timedelta, timezone
+from app.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+ 
 
 def hash_password(password: str):
     return pwd_context.hash(password)
@@ -8,3 +20,54 @@ def hash_password(password: str):
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    user_dict = get_user_dict(user,username )
+    
+    if not user:
+        return False
+    if not verify_password(password, user[username]["hashed_password"]):
+        return False
+    return user_dict
+
+def get_user_dict(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+    
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(login=token_data.username)
+    user_dict = get_user_dict(user,token_data.username )
+    if user is None:
+        raise credentials_exception
+    return user_dict
+
+def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
